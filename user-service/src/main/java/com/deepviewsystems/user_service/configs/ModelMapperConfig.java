973c3943ff67edd6159fb9d8aeae95669c3891f4
FedeGonzalez <142946367+FedericoGonzalezGiordano@users.noreply.github.com>
2025-07-30 18:46:51 -0300

    ADD: add configs

---
+++ user-service/src/main/java/com/deepviewsystems/user_service/configs/ModelMapperConfig.java
@@ -0,0 +1,36 @@
+package com.deepviewsystems.user_service.configs;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        
+        ModelMapper modelMapper = new ModelMapper();
+        
+        // Configuración para mapeo estricto
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setSkipNullEnabled(true)
+                .setAmbiguityIgnored(true);
+        
+        return modelMapper;
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
+    }
+} 
