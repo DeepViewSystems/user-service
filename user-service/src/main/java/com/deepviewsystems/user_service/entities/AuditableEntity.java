fa6d901a92807e7d93cac5ef19b0ab831eedc295
FedeGonzalez <142946367+FedericoGonzalezGiordano@users.noreply.github.com>
2025-07-31 11:45:42 -0300

    chg: Enable manual trigger for CI pipeline and clean up code
    Added 'workflow_dispatch' to allow manual triggering of the CI pipeline. Also removed a stray comment from AuditableEntity.java for code cleanliness.

--- user-service/src/main/java/com/deepviewsystems/user_service/entities/AuditableEntity.java
+++ user-service/src/main/java/com/deepviewsystems/user_service/entities/AuditableEntity.java
@@ -12,7 +12,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
-// test
+
 @Getter
 @Setter
 @MappedSuperclass 
